cb507224f74d03755c767838dd982612e26a29bf
Dan <111394311+Dannzt@users.noreply.github.com>
2025-03-13 13:21:51 -0300

    Create matrizTransposta.js

---
+++ Atividade 2/matrizTransposta.js
@@ -0,0 +1,9 @@
+/* A função deve receber uma matriz (array de arrays) A e imprimir a matriz original e a versão transposta; A imagem abaixo aparenta um exemplo de transposição de matriz:
+
+   [1, 2],          
+a= [3, 4],    ===>  at=  [ 1  3  5 ]
+   [5, 6]                [ 2  4  6 ]
+ 
+ */
+
+
