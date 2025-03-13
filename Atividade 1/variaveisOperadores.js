255fdf1857633c181af4bb8145e6c31dd711ed00
Dan <111394311+Dannzt@users.noreply.github.com>
2025-03-13 20:45:32 -0300

    Update variaveisOperadores.js
    att atividade

--- Atividade 1/variaveisOperadores.js
+++ Atividade 1/variaveisOperadores.js
@@ -23,4 +23,4 @@ console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${num_C}`
  * Divisão
  */
 num_C = num_A / num_B;
-console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${num_C}`);
+console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${num_C}`); 
