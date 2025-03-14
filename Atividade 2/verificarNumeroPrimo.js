a3bedb9fd26e9dd73bad3a252cc14e73eafc205d
Dannzt <danielribeiroteles021@gmail.com>
2025-03-14 10:37:52 -0300

    add atividade 2 + aula em sala

--- Atividade 2/verificarNumeroPrimo.js
+++ Atividade 2/verificarNumeroPrimo.js
@@ -1,4 +1,10 @@
 /*
+No seu repositório de atividades no github, crie uma pasta atividade2.
+
+Dentro dessa pasta, crie o arquivo verificarNumeroPrimo.js;
+
+Implemente uma função com a seguinte assinatura: verificarNumeroPrimo(n);
+
 A função deve verificar se o número "n" recebido é primo e retornar true, caso verdadeiro, ou false caso não seja primo.
 
 No mesmo arquivo, faça a chamada da função para os seguintes casos de teste:
@@ -15,4 +21,32 @@ verificarNumeroPrimo(104729); //true
 verificarNumeroPrimo(14348907); //false
 
 */
+function verificarNumeroPrimo(n){
+    if(n <= 1){
+        return false;
+    }
 
+    if(n % 2 == 0 && n != 2){
+        return false;
+    }
+
+    for(let i=3; i < n; i+=2){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+console.log(`verificarNumeroPrimo(0)=${verificarNumeroPrimo(0)}`); //false
+console.log(`verificarNumeroPrimo(1)=${verificarNumeroPrimo(1)}`); //false
+console.log(`verificarNumeroPrimo(2)=${verificarNumeroPrimo(2)}`); //true
+console.log(`verificarNumeroPrimo(3)=${verificarNumeroPrimo(3)}`); //true
+console.log(`verificarNumeroPrimo(7)=${verificarNumeroPrimo(7)}`); //true
+console.log(`verificarNumeroPrimo(83)=${verificarNumeroPrimo(83)}`); //true
+console.log(`verificarNumeroPrimo(100)=${verificarNumeroPrimo(100)}`); //false
+console.log(`verificarNumeroPrimo(991)=${verificarNumeroPrimo(991)}`); //true
+console.log(`verificarNumeroPrimo(104729)=${verificarNumeroPrimo(104729)}`); //true
+console.log(`verificarNumeroPrimo(14348907)=${verificarNumeroPrimo(14348907)}`); //false
