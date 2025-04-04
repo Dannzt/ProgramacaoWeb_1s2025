044376549546fc3d70b52549c7d3477650d975b3
Dan <111394311+Dannzt@users.noreply.github.com>
2025-04-04 09:56:14 -0300

    correção em sala atividade 4

--- Atividade 4/index.js
+++ Atividade 4/index.js
@@ -1,50 +1,40 @@
-//para inicializar, usa os comandos no cmd (identifica a pasta correta, depois "npm init" e depois "npm install express@4.21.2" para ativar o node) //testa em casa
-
 const express = require('express');
-const estoque = require('./estoque')
-const app = express();
+const estoque = require('./estoque');
+const app     = express();
 
-app.get('/',(req,res)=>{
-    let html = '<h1> Loja de camisas doFLAMENGO </h1>'
-    html += '<h3>rotas disponiveis</h3>'
-    html += '<p>/adicionar/:id/:nome/:qtd</p>'
-    html += '<p>/listar</p>'
-    html += '<p>/remover/:id</p>'
-    html += '<p>/editar/:id/:qtd</p>'
-    res.send(html)
-})
+app.get('/', (req, res)=>{
+    let html =  '<h1>app_estoque</h1>';
+    html     += '<h3>Rotas disponíveis:</h3>'
+    html     += '<p>/adicionar/:id/:nome/:qtd</p>'
+    html     += '<p>/listar</p>'
+    html     += '<p>/remover/:id</p>'
+    html     += '<p>/editar/:id/:qtd</p>'
+    res.send(html);
+});
 
-// /adicionar/:id/:nome/:qtd
 app.get('/adicionar/:id/:nome/:qtd', (req, res)=>{
-    const id   = Number(req.params.id);
-    const nome = req.params.nome;
-    const qtd  = Number(req.params.qtd);
-
-    const item = {
-        id: id,
-        nome: nome,
-        qtd: qtd
-    }
+    let item = {
+        id: Number(req.params.id),
+        nome: req.params.nome,
+        qtd: Number(req.params.qtd)
+    };
 
     res.send(estoque.adicionar(item));
 });
 
-// /listar
 app.get('/listar', (req, res)=>{
-   res.send(estoque.listar());
+    res.send(estoque.listar());
 });
 
-// /remover/:id
 app.get('/remover/:id', (req, res)=>{
-
+    
 });
 
-// /editar/:id/:qtd
 app.get('/editar/:id/:qtd', (req, res)=>{
-   const id  = Number(req.params.id);
-   const qtd = Number(req.params.qtd);
-   
-   res.send(estoque.editar(id, qtd));
+    let id  = Number(req.params.id);
+    let qtd = Number(req.params.qtd);
+
+    res.send(estoque.editar(id, qtd));
 });
 
 const PORT = 8080;
